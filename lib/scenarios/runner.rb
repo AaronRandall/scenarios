105aa91f9deec147624985206237364d8a7cae05
Aaron Randall <aaronjrandall@gmail.com>
2013-04-21 18:10:37 +0100

    Added automation library, deployment library, and logic to run test steps in runner.rb.

--- lib/scenarios/runner.rb
+++ lib/scenarios/runner.rb
@@ -1,14 +1,13 @@
 module Scenarios
   require ROOT + 'devices'
 
-  TUNEUP_BIN   = ROOT + 'tuneup/test_runner/run'
-  TESTS_PATH   = ROOT + 'tests'
-  OUTPUT_PATH  = ROOT + 'output'
-  SCRIPT_PATH  = ROOT + 'bin/scripts'
+  AUTOMATION_LIBRARY = ROOT + 'automation_library/tuneup/test_runner/run'
+  SCRIPT_PATH        = ROOT + 'bin/scripts'
 
   class Runner
     def initialize(params)
       @ios_app_path      = params[:ios_app_path]
+      @tests_path        = params[:tests_path]
       @test_name         = params[:test_name]
       @run_on_hardware   = params[:run_on_hardware]
       @hardware_id       = params[:hardware_id]
@@ -24,29 +23,32 @@ module Scenarios
       puts "In run for app path=#{@ios_app_path}"
 
       tests_to_run = []
-      if OPTS[:test_name]
-        # Run a single test supplied by the user
-        tests_to_run.push("#{TESTS_PATH}/#{OPTS[:test_name]}")
+      if @test_name
+        tests_to_run.push("#{@tests_path}/#{@test_name}")
       else
-        # Run all tests
-        Dir["#{TESTS_PATH}/*_test.js"].each {|test| tests_to_run.push(test) }
+        Dir["#{@tests_path}/*_test.js"].each {|test| tests_to_run.push(test) }
       end
 
-      # Determine the target to run the tests on
-      if OPTS[:run_on_hardware] or OPTS[:hardware_id]
-        test_device = Hardware.new(tests_to_run, OPTS[:test_variables], OPTS[:hardware_id])
+      if @run_on_hardware or @hardware_id
+        test_device = Devices::Hardware.new(tests_to_run, @test_variables, @hardware_id)
       else
-        test_device = Simulator.new(tests_to_run, OPTS[:test_variables])
+        test_device = Devices::Simulator.new(tests_to_run, @test_variables)
       end
 
+      run_test_steps(test_device) 
+    end
+
+    private
+    
+    def run_test_steps(test_device)
       # kill any stale sim/instrument processes
       kill_simulator_and_instruments
 
-      test_device.clean_target                  if OPTS[:clean_target]
-      test_device.clean_project_build_directory if OPTS[:clean_project_build_directory]
-      test_device.build_app                     if OPTS[:build_app]
-      test_device.install_app                   if OPTS[:install_app]
-      test_device.run_tests                     if OPTS[:run_tests]
+      test_device.clean_target                  if @clean_target
+      test_device.clean_project_build_directory if @clean_project_build_directory
+      test_device.build_app                     if @build_app
+      test_device.install_app                   if @install_app
+      test_device.run_tests                     if @run_tests
     end
 
     def kill_simulator_and_instruments
@@ -55,5 +57,10 @@ module Scenarios
       log('Killing instruments (if running)')
       system "killall 'instruments'"
     end
+
+    def log(message)
+      puts "[INFO]: #{message}"
+    end
+
   end
 end
